07275f690adf38d0832fc469f2a99de63ac2d08c
XcQuid <creed.35@yandex.ru>
2022-03-01 17:53:53 +0300

    refactor: change popup classnames

--- src/components/ImagePopup/ImagePopup.js
+++ src/components/ImagePopup/ImagePopup.js
@@ -1,8 +1,8 @@
 export default function ImagePopup({card, isOpen, onClose}) {
-  const popupClassName = (isOpen ? 'popup_opened' : '');
+  const popupClassName = (`popup popup_type_image ${isOpen ? 'popup_opened' : ''}`);
 
   return (
-    <div className={`popup popup_type_image ${popupClassName}`} >
+    <div className={popupClassName} >
       <div className="popup__image-container">
         <button className="button popup__close-button" type="button" aria-label="Закрыть" onClick={onClose}></button>
         <figure className="popup__figure">
