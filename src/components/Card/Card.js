4f32529567721f5bdef52d48a7699fd49e60efd1
XcQuid <creed.35@yandex.ru>
2022-03-01 17:55:52 +0300

    feat: add context to the Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,18 +8,23 @@ export default function Card({card, onCardClick}) {
 
   const currentUser = React.useContext(CurrentUserContext);
   const isOwn = currentUser._id === card.owner._id;
-  const cardDeleteButtonClassNmae = (
-    `card__delete-button ${isOwn ? '' : 'card__delete-button_hidden'}`
-  ); 
+  const cardDeleteButtonClassName = (
+    `button card__delete-button ${isOwn ? '' : 'card__delete-button_hidden'}`
+  );
+
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = (
+    `button card__like-button ${isLiked ? 'card__like-button_active' : ''}`
+  );
 
   return (
     <li className="card">
       <img src={card.link} alt={card.name} className="card__image" onClick={handleClick}/>
-      <button className={`button card__delete-button ${cardDeleteButtonClassNmae}`} type="button" aria-label="Удалить"></button>
+      <button className={cardDeleteButtonClassName} type="button" aria-label="Удалить"></button>
       <div className="card__description">
         <h2 className="card__title">{card.name}</h2>
         <div className="card__like">
-          <button className="button card__like-button" type="button" aria-label="Нравится"></button>
+          <button className={cardLikeButtonClassName} type="button" aria-label="Нравится"></button>
           <div className="card__like-counter">{card.likes.length}</div>
         </div>
       </div>
